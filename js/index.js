9413f98f12ddd4a376137a861512f298170d1ff5
Lidia <maciellili@hotmail.com>
2021-11-22 11:41:07 -0300

    valiidacao login

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let email = document.getElementById("login-_email");
+let senha = document.getElementById("login_senha");
+
+
+senha.onblur = function() {
+    let regexSenha = /^.{8,}$/;
+    let msgSenhaError = document.querySelector(".senha-msg-error");
+    let msgSenhaOk = document.querySelector(".senha-msg-success");
+
+    if (regexSenha.test(senha.value)) {
+        msgSenhaError.style.display = "none";
+        msgSenhaOk.style.color = "green";
+        msgSenhaOk.style.display = "block";
+    } else {
+        msgSenhaOk.style.display = "none";
+        msgSenhaError.style.color = "red";
+        msgSenhaError.style.display = "block";
+    }
+}
+
